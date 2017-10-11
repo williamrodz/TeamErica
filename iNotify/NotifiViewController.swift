5362fd0b5561996cefcfe916214564fb705f00eb
Mesert Kebed <mkebede@mit.edu>
2017-10-11 12:28:08 -0400

    created NotfiViewController

---
+++ iNotify/NotifiViewController.swift
@@ -0,0 +1,38 @@
+//
+//  NotifiViewController.swift
+//  iNotify
+//
+//  Created by Meseret  Kebede on 11/10/2017.
+//  Copyright © 2017 Team Erica. All rights reserved.
+//
+
+import UIKit
+
+class NotifiViewController: UIViewController {
+
+    @IBAction func button(_ sender: Any) {
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
