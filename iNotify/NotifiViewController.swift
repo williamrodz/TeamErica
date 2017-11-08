94f52fafbab31ba707b247f1c04188d4ac797b16
Jenny Jin <jennyj5@mit.edu>
2017-11-08 14:15:00 -0500

    Email feature implemented for "Send to Jenny"

--- iNotify/NotifiViewController.swift
+++ iNotify/NotifiViewController.swift
@@ -24,7 +24,40 @@ class NotifiViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
-
+    @IBAction func sendEmail(_ sender: Any) {
+        
+        let smtpSession = MCOSMTPSession()
+        smtpSession.hostname = "smtp.gmail.com"
+        smtpSession.username = "jennyjin43@gmail.com"
+        smtpSession.password = "newbeginnings<3"
+        smtpSession.port = 465
+        smtpSession.authType = MCOAuthType.saslPlain
+        smtpSession.connectionType = MCOConnectionType.TLS
+        smtpSession.connectionLogger = { (connectionID, type, data) in
+            if data != nil{
+                if let string = NSString(data: data!, encoding: String.Encoding.utf8.rawValue){
+                    NSLog("Connectionlogger: \(string)")
+                }
+            }
+        }
+        let builder = MCOMessageBuilder()
+        builder.header.to = [MCOAddress(displayName: "Jenny", mailbox: "jennyjin43@gmail.com")]
+        builder.header.from = MCOAddress(displayName: "Jenny", mailbox: "jennyjin43@gmail.com")
+        builder.header.subject = "Test Email"
+        builder.htmlBody="<p>This is a test using MailCore</p>"
+        
+        let rfc822Data = builder.data()
+        let sendOperation = smtpSession.sendOperation(with: rfc822Data)
+        sendOperation?.start { (error) -> Void in
+            if (error != nil) {
+                NSLog("Error sending email: \(error)")
+            } else {
+                NSLog("Successfully sent email!")
+            }
+            
+        }
+    }
+    
     /*
     // MARK: - Navigation
 
