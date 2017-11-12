497aa101128441fe2be98a700ed16a541bf55fe6
William Rodriguez <wrod@mit.edu>
2017-11-12 17:08:44 -0500

    Added sendEmail_bridge.h file to GitHub

---
+++ iNotify/sendEmail_bridge.h
@@ -0,0 +1,13 @@
+//
+//  sendEmail_bridge.h
+//  iNotify
+//
+//  Created by Jenny Jin on 11/8/17.
+//  Copyright © 2017 Team Erica. All rights reserved.
+//
+
+#ifndef sendEmail_bridge_h
+#define sendEmail_bridge_h
+#include "MailCore.h"
+
+#endif /* sendEmail_bridge_h */
