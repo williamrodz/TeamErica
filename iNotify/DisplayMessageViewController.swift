33badd3223326099e22b53fdfe46b080f9bbd619
William Rodriguez <wrod@mit.edu>
2017-12-02 21:02:33 -0500

    Prevented screen from going to least for 20 minutes in Display screen. Also added Email Settings view controller and storyboard item but no logic yet.

--- iNotify/DisplayMessageViewController.swift
+++ iNotify/DisplayMessageViewController.swift
@@ -18,10 +18,23 @@ class DisplayMessageViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
         print("Display message in appSettings is \(appSettings.getDisplayMessage())")
         displayMessageLabel.text =  appSettings.getDisplayMessage()
-        print("Loading from DisplayMessageViewController")
+        displayMessageLabel.transform = CGAffineTransform(rotationAngle: CGFloat.pi / 2)
+
+        
+        
+        let secondsinOneMinute:Int = 60
+        UIApplication.shared.isIdleTimerDisabled = true // Stop screen from going to sleep
+        let desiredMinutesBeforeSleep:Int = 20
+        let when = DispatchTime.now() +  .seconds(secondsinOneMinute*desiredMinutesBeforeSleep) // change 2 to desired number of seconds
+        print("Will wait \(desiredMinutesBeforeSleep) minutes before re-enabling sleep")
+
+        DispatchQueue.main.asyncAfter(deadline: when) {
+            UIApplication.shared.isIdleTimerDisabled = false // Re-enable screen to go to sleep
+        }
+    
+
     }
 
     override func didReceiveMemoryWarning() {
