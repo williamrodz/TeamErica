8930bf8a22249b058d88a0340f3acd896dbb8640
William Rodriguez <wrod@mit.edu>
2018-09-13 15:41:49 -0400

    Updated logic of sleep timer in display message view controller so that sleep timer is disabled here

--- iNotify/DisplayMessageViewController.swift
+++ iNotify/DisplayMessageViewController.swift
@@ -24,15 +24,19 @@ class DisplayMessageViewController: UIViewController {
 
         
         // logic to prevent the screen from going to sleep when in display mode
-        let secondsinOneMinute:Int = 60
-        UIApplication.shared.isIdleTimerDisabled = true // Stop screen from going to sleep
-        let desiredMinutesBeforeSleep:Int = 20
-        let when = DispatchTime.now() +  .seconds(secondsinOneMinute*desiredMinutesBeforeSleep) // change 2 to desired number of seconds
-        print("Will wait \(desiredMinutesBeforeSleep) minutes before re-enabling sleep")
-
-        DispatchQueue.main.asyncAfter(deadline: when) {
-            UIApplication.shared.isIdleTimerDisabled = false // Re-enable screen to go to sleep
-        }
+        
+        //1st attempt: set timer to re-enable sleep
+//        let secondsinOneMinute:Int = 60
+//        UIApplication.shared.isIdleTimerDisabled = true // Stop screen from going to sleep
+//        let desiredMinutesBeforeSleep:Int = 20
+//        let when = DispatchTime.now() +  .seconds(secondsinOneMinute*desiredMinutesBeforeSleep) // change 2 to desired number of seconds
+//        print("Will wait \(desiredMinutesBeforeSleep) minutes before re-enabling sleep")
+//
+//        DispatchQueue.main.asyncAfter(deadline: when) {
+//            UIApplication.shared.isIdleTimerDisabled = false // Re-enable screen to go to sleep
+//        }
+        // 2nd attempt: just disable sleep mode
+        UIApplication.shared.isIdleTimerDisabled = true
     
 
     }
@@ -44,5 +48,11 @@ class DisplayMessageViewController: UIViewController {
     override func viewWillAppear(_ animated: Bool) {
         navigationItem.title = "Display"
     }
+    
+    // The following is added to re-enable the sleep timer when the client leaves the display message view controller
+    override func viewWillDisappear(_ animated: Bool) {
+        UIApplication.shared.isIdleTimerDisabled = false
+    }
+    
 
 }
